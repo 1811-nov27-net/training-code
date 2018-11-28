3bd2c765c10dcd66f06a2da1bac943117fdda6b2
Nicholas Escalona <nicholas.escalona@gmail.com>
2018-11-28 11:57:34 -0600

    make hello world visual studio app.

---
+++ week1/day3/HelloVisualStudio/HelloVisualStudioApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloVisualStudioApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // F5, Ctrl+F5 to run your code
+            // File -> New Project to make new projects
+            //   be sure not to have the "new repo" box checked
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

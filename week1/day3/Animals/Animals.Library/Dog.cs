c8163545c3612184ea514039c96261f06cb69c1d
Nicholas Escalona <nicholas.escalona@gmail.com>
2018-11-28 14:34:15 -0600

    add Animals soln with class demos.

---
+++ week1/day3/Animals/Animals.Library/Dog.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Animals.Library {
+    public class Dog {
+        // not fields - properties
+        public string Name { get; set; }
+
+        // auto-property
+        public string Breed { get; set; }
+
+        // property (with explicit backing field)
+        private int _age;
+        public int Age {
+            get {
+                return _age;
+            }
+            set {
+                _age = value;
+                // keyword "value" in a setter takes in the assigned value
+            }
+        }
+
+        // classic getters and setters
+        private int Weight;
+
+        public int GetWeight () {
+            return Weight;
+        }
+
+        public void SetWeight (int weight) {
+            Weight = weight;
+        }
+
+        public void Bark()
+        {
+            Console.WriteLine("Woof");
+        }
+
+        // snippets: prop, propfull
+    }
+}
---
+++ week1/day3/Animals/Animals.UI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Animals.Library;
+
+namespace Animals.UI
+{
+    class Program
+    {
+        // your entry point needs a static void Main(string[] args) method
+        // and that is where the execution starts
+
+        // "Program.cs" and Program class name are just conventions.
+
+        // naming convention in C# - PascalCase aka TitleCase for
+        //    classes, methods, properties, namespaces
+        // camelCase (first letter lowercase) for local variables.
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Bark();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

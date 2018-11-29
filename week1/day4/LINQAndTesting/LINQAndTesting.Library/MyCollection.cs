3670997fafbfc4109826868f3bbe45a4132007f5
Nicholas Escalona <nicholas.escalona@gmail.com>
2018-11-29 15:32:01 -0600

    add LINQ, testing demo with failing tests.

---
+++ week1/day4/LINQAndTesting/LINQAndTesting.Library/MyCollection.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LINQAndTesting.Library
+{
+    /// <summary>
+    /// a list with some extra helper methods.
+    /// </summary>
+    /// <remarks>
+    /// two strategies we could use to leverage the builtin collection classes:
+    /// - inheritance (MyCollection IS A List)
+    /// - composition (MyCollection HAS A List)
+    /// </remarks>
+    public class MyCollection
+    {
+        // "readonly" just means i can't reassign "_list" to a different object
+        // later. it doesn't mean i can't modify the object with its methods.
+        private readonly List<string> _list = new List<string>();
+
+        // every class has at least one constructor.
+        // if you don't define one, it has a default constructor without any
+        //   parameters -
+        //   "public MyCollection() {}"
+        // but, as soon as you define any constructor, that default one will not
+        //    be added.
+
+        public void Sort()
+        {
+            _list.Sort();
+        }
+
+        public void Add(string item)
+        {
+            _list.Add(item);
+        }
+
+        // property without a "set"
+        // calling code can say "coll.Length" instead of coll.GetLength()
+        public int Length
+        {
+            get
+            {
+                return _list.Count;
+            }
+        }
+
+        public string Get(int index)
+        {
+            return _list[index];
+        }
+
+        public string Longest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ week1/day4/LINQAndTesting/LINQAndTesting.Tests/MyCollectionTests.cs
@@ -0,0 +1,63 @@
+using LINQAndTesting.Library;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace LINQAndTesting.Tests
+{
+    // typically one test class to test each real class
+    public class MyCollectionTests
+    {
+        // in XUnit, each unit test, to test one thing, should be
+        // a method with the "Fact" attribute.
+
+        [Fact] // this kind of thing is called an "attribute" -
+               //    special kind of class that adds extra behavior to a class,
+               //    method, property, etc.
+        public void EmptyCollectionShouldHaveZeroLength()
+        {
+            // arrange (set up the situation to be tested)
+            // sometimes people use acronym "SUT" for "subject under test"
+            var sut = new MyCollection();
+            // it's already empty
+
+            // act (run the method/behavior that we're specifically testing)
+            var result = sut.Length;
+
+            // assert (define what the correct result is and check that we got it.)
+            Assert.Equal(0, result);
+            // Assert class has lots of static methods to check various things
+        }
+
+        // [Fact] is for tests that don't take any parameters.
+        // [Theory] is a convenient way to run a parameterized test with
+        //     more than one set of data. (don't repeat yourself)
+
+        [Theory]
+        [InlineData(new string[] { "a", "ab" }, "ab")]
+        [InlineData(new string[] { "ab", "a" }, "ab")]
+        [InlineData(new string[] { "a" }, "a")]
+        [InlineData(new string[] { }, null)]
+        [InlineData(new string[] { "ab", "b2" }, "ab")]
+        [InlineData(new string[] { "ab", null, "a" }, "ab")]
+        public void LongestShouldReturnLongest(string[] items, string expected)
+        {
+            // arrange
+            var coll = new MyCollection();
+            foreach (var item in items)
+            {
+                coll.Add(item);
+            }
+
+            // act
+            string actual = coll.Longest();
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        // test-driven development:
+        // step 1: write test(s) that fail
+        // step 2: write the code to make the test(s) pass.
+    }
+}

7da0136af351f33d9f09c1bbfca3cc3a5d08524b
Nicholas Escalona <nicholas.escalona@gmail.com>
2018-12-06 14:13:03 -0600

    consume moviesdb from ef demo console app.

--- week2/day4/EFDBFirstDemo/EFDBFirstDemo.UI/Program.cs
+++ week2/day4/EFDBFirstDemo/EFDBFirstDemo.UI/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using EFDBFirstDemo.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace EFDBFirstDemo.UI
 {
@@ -6,7 +10,22 @@ namespace EFDBFirstDemo.UI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var connectionString = SecretConfiguration.ConnectionString;
+
+            var optionsBuilder = new DbContextOptionsBuilder<MoviesDBContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+            var options = optionsBuilder.Options;
+
+            var movies = new List<Movie>();
+            using (var db = new MoviesDBContext(options))
+            {
+                movies = db.Movie.ToList();
+            }
+
+            foreach (var item in movies)
+            {
+                Console.WriteLine($"movie ID {item.Id}, name {item.Name}");
+            }
         }
     }
 }

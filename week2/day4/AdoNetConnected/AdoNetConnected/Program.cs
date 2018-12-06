0d67d6520adc98bb64e347e7aa536bcf6e7dbb1c
Nicholas Escalona <nicholas.escalona@gmail.com>
2018-12-06 12:18:19 -0600

    add disconnected arch example.

--- week2/day4/AdoNetConnected/AdoNetConnected/Program.cs
+++ week2/day4/AdoNetConnected/AdoNetConnected/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 
 namespace AdoNetConnected
 {
@@ -6,7 +7,62 @@ namespace AdoNetConnected
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // ADO.NET is technically MS's umbrella name for all data-related libraries
+            // including Entity Framework
+            // but when we say "ADO.NET" we typically are talking about the older
+            // ways of doing things with DataReader, DataAdapter objects.
+
+            // in various GUIs, you need the server URL, login, password.
+            // in code, we have developed a convention to use what we call a
+            // "connection string" which will jam all that data into
+            // one string to connect to some kind of data source, in our case, SQL Server.
+
+            // never commit your connection strings to source control like git.
+            // they're basically passwords
+
+            var connectionString = SecretConfiguration.ConnectionString;
+
+            Console.WriteLine("Enter name of movie: ");
+            var input = Console.ReadLine();
+            var commandString = $"SELECT * FROM Movies.Movie WHERE Name = '{input}';";
+            // allows "SQL injection"
+            // un-sanitized user input must not be used to construct SQL queries
+            // directly, or else hackers can access or destroy things
+
+            // connected architecture - we're going to receive the whole result
+            // have it waiting in the network buffer
+            // and use a "cursor"/iterator to read it in row by row.
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                // connected architecture
+                
+                // step 1: open the connection
+                connection.Open();
+                // step 2: execute the query
+                using (var command = new SqlCommand(commandString, connection))
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    // command.ExecuteReader for SELECT queries that return things
+                    //    (returns a DataReader)
+                    // command.ExecuteNonQuery for all other commands (that don't return things)
+                    //    (returns an int for number of rows affected)
+
+                    // step 3: process results
+                    if (reader.HasRows)
+                    {
+                        // for each row...
+                        while (reader.Read())
+                        {
+                            object id = reader["ID"]; // access values by column name
+                            object name = reader["Name"];
+                            Console.WriteLine($"ID: {id}, Name: {name}");
+                        }
+                    }
+                }
+                // step 4: close the connection
+                connection.Close();
+            }
         }
     }
 }
---
+++ week2/day4/AdoNetDisconnected/AdoNetDisconnected/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AdoNetDisconnected
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = SecretConfiguration.ConnectionString;
+
+            Console.WriteLine("Enter name of movie: ");
+            var input = Console.ReadLine();
+            var commandString = $"SELECT * FROM Movies.Movie WHERE Name = '{input}';";
+            // allows "SQL injection"
+
+            // disconnected architecture - we're going to wait to get the whole result,
+            // load it into a "DataSet" (in-memory collection), close our connection
+            // and THEN process the results.
+
+            // this has more overhead on the C# side, but it keeps the connection open
+            // for less time (which is really good because the DB is usually the bottleneck)
+
+            // dataset will hold our results
+            var dataSet = new DataSet();
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                // disconnected architecture
+
+                // step 1: open the connection
+                connection.Open();
+                using (var command = new SqlCommand(commandString, connection))
+                using (var adapter = new SqlDataAdapter(command))
+                {
+                    // step 2: execute the query, filling dataset
+                    adapter.Fill(dataSet);
+                    // (still uses DataReader object internally)
+                }
+                // step 3: close the connection
+                connection.Close();
+
+                // step 4: process results
+                DataTable firstTable = dataSet.Tables[0];
+                // watch out - foreach without generics does a cast when you assign the
+                // type right here (DataRow)
+                foreach (DataRow row in firstTable.Rows)
+                {
+                    // DataSet contains DataTable, DataColumn, DataRow, etc.
+                    object id = row["ID"]; // access values by column name
+                    object name = row["Name"];
+                    Console.WriteLine($"ID: {id}, Name: {name}");
+                }
+            }
+        }
+    }
+}

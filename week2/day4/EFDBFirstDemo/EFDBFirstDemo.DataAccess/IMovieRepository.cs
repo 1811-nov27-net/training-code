b58f31ba672f21c0a4a6c874722fbdc6de073c70
Nicholas Escalona <nicholas.escalona@gmail.com>
2018-12-06 14:53:11 -0600

    add movie repository class without implementation

---
+++ week2/day4/EFDBFirstDemo/EFDBFirstDemo.DataAccess/IMovieRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFDBFirstDemo.DataAccess
+{
+    public interface IMovieRepository
+    {
+        // the repository pattern abstracts the details of data access to its consumers
+        // it will provide simple "CRUD" type methods
+        //   Create, Read, Update, Delete
+        // and hide the details of which database, even is it a database, are we using ADO.NET,
+        // are we using EntityFramework, etc.
+
+        IList<Movie> GetAllMovies();
+
+        IList<Movie> GetAllMoviesWithGenres();
+
+        Movie GetMovieById(int id);
+
+        Movie GetMovieByName(string name);
+
+        void DeleteMovie(Movie movie);
+
+        void EditMovie(Movie movie);
+
+        void CreateMovie(Movie movie);
+    }
+}
---
+++ week2/day4/EFDBFirstDemo/EFDBFirstDemo.DataAccess/MovieRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFDBFirstDemo.DataAccess
+{
+    public class MovieRepository : IMovieRepository
+    {
+        public void CreateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        // based on the movie's ID, we're going to update the DB's movie
+        // with this one's property values.
+        public void EditMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Movie> GetAllMovies()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Movie> GetAllMoviesWithGenres()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Movie GetMovieById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Movie GetMovieByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

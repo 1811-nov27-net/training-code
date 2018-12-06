6a37cedd1d1924ed3e0da0e05d5255e5315919e8
Nicholas Escalona <nicholas.escalona@gmail.com>
2018-12-06 13:59:36 -0600

    scaffold dbcontext from moviesdb and add db first directions

---
+++ week2/day4/EFDBFirstDemo/EFDBFirstDemo.DataAccess/MoviesDBContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EFDBFirstDemo.DataAccess
+{
+    public partial class MoviesDBContext : DbContext
+    {
+        public MoviesDBContext()
+        {
+        }
+
+        public MoviesDBContext(DbContextOptions<MoviesDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Genre> Genre { get; set; }
+        public virtual DbSet<Movie> Movie { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.0-rtm-35687");
+
+            modelBuilder.Entity<Genre>(entity =>
+            {
+                entity.ToTable("Genre", "Movies");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Movie>(entity =>
+            {
+                entity.ToTable("Movie", "Movies");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.GenreId).HasColumnName("GenreID");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.HasOne(d => d.Genre)
+                    .WithMany(p => p.Movie)
+                    .HasForeignKey(d => d.GenreId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Movies_Genre_GenreID");
+            });
+        }
+    }
+}

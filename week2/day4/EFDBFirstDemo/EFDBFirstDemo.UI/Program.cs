05de99861b53e37aa347fb98ff661623f3b7f118
Nicholas Escalona <nicholas.escalona@gmail.com>
2018-12-06 13:40:04 -0600

    add project skeleton for efdbfirst demo.

---
+++ week2/day4/EFDBFirstDemo/EFDBFirstDemo.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFDBFirstDemo.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

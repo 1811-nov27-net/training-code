113d07d53b8c389db0e8e20a401b182066f83d35
Nicholas Escalona <nicholas.escalona@gmail.com>
2018-12-13 11:40:24 -0600

    migrate database

--- week3/day3/MVCDemo/MVCDemo.DataAccess/MovieCastMemberJunction.cs
+++ week3/day3/MVCDemo/MVCDemo.DataAccess/MovieCastMemberJunction.cs
@@ -7,9 +7,13 @@ namespace MVCDemo.DataAccess
     // implementing many-to-many relationship
     public class MovieCastMemberJunction
     {
+        // primary key of the junction table
+        // (by convention, even without [Key])
         public int Id { get; set; }
 
         public virtual Movie Movie { get; set; }
         public virtual CastMember CastMember { get; set; }
+        // navigation properties imply foreign keys
+        // when we do migrations / ensurecreated.
     }
 }
--- week3/day3/MVCDemo/MVCDemo.DataAccess/MovieDBContext.cs
+++ week3/day3/MVCDemo/MVCDemo.DataAccess/MovieDBContext.cs
@@ -10,6 +10,11 @@ namespace MVCDemo.DataAccess
 
         // with EF code-first, when our data model needs to change, we make changes to the classes/dbcontext
         //   then run migrations to update the database without destroying it.
+        //   to run migrations - startup project needs "Microsoft.EntityFrameworkCore.Tools"
+        //   then run in PM Console, with "default project" set to the project with your dbcontext in it,
+        //     Add-Migration <some name describing the change>
+        //     Update-Database
+
         // if we used EnsureCreated, we can't use migrations, need to start over
 
         public MovieDBContext(DbContextOptions<MovieDBContext> options) : base(options)

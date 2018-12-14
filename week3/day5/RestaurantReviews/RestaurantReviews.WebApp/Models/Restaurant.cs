d83de4e29355fdcb8905118cfecb25ca6fe6d729
Nicholas Escalona <nicholas.escalona@gmail.com>
2018-12-14 10:54:27 -0600

    add restaurantreviews example with mvc web app.

---
+++ week3/day5/RestaurantReviews/RestaurantReviews.WebApp/Models/Restaurant.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace RestaurantReviews.WebApp.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<Review> Reviews { get; set; }
+    }
+}
---
+++ week3/day5/RestaurantReviews/RestaurantReviews.WebApp/Models/Review.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantReviews.WebApp.Models
+{
+    public class Review
+    {
+        // the HTML/tag helpers like "DisplayNameFor"
+        // will use this instead of the property's name
+        [Display(Name = "Reviewer Name")]
+        public string ReviewerName { get; set; }
+        public int? Score { get; set; }
+        public string Text { get; set; }
+    }
+}

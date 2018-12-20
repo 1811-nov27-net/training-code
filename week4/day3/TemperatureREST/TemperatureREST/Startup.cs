ca39902f3070a7aae72e2f15e22fcbea8197836b
Nicholas Escalona <nicholas.escalona@gmail.com>
2018-12-20 16:00:20 -0600

    add swagger ui to temperatureREST

--- week4/day3/TemperatureREST/TemperatureREST/Startup.cs
+++ week4/day3/TemperatureREST/TemperatureREST/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace TemperatureREST
 {
@@ -29,6 +30,12 @@ namespace TemperatureREST
                 .AddMvc()
                 .AddXmlSerializerFormatters() // adds support for XML serialize & deserialize
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // add Swashbuckle Swagger UI middleware
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "TempRecord API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +50,16 @@ namespace TemperatureREST
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "TempRecord API v1");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
